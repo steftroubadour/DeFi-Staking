e5ac7af38b841cbd4ba085ceed3dda31c1a38ef2
steftroubadour <steftroubadour@gmail.com>
2021-12-07 11:28:12 +0100

    Updates

---
+++ precache-manifest.76d9a0c1fa86aec10ec1a013181b165a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5fc17787eedeffd18210e4ec71d0bb",
+    "url": "/steftroubadour/DeFi-Staking/index.html"
+  },
+  {
+    "revision": "4208c3c344d2a172b062",
+    "url": "/steftroubadour/DeFi-Staking/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "6527fcfe06b0b2412f4c",
+    "url": "/steftroubadour/DeFi-Staking/static/css/main.c9612da4.chunk.css"
+  },
+  {
+    "revision": "4208c3c344d2a172b062",
+    "url": "/steftroubadour/DeFi-Staking/static/js/2.025e7f16.chunk.js"
+  },
+  {
+    "revision": "6527fcfe06b0b2412f4c",
+    "url": "/steftroubadour/DeFi-Staking/static/js/main.d0481e79.chunk.js"
+  },
+  {
+    "revision": "e37ca79824f77d728895",
+    "url": "/steftroubadour/DeFi-Staking/static/js/runtime-main.8021f661.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/steftroubadour/DeFi-Staking/precache-manifest.76d9a0c1fa86aec10ec1a013181b165a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/steftroubadour/DeFi-Staking/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
